a5468428c494282e431facb9c29259703f546911
John Darel De Chavez <johndarel0914@gmail.com>
2023-05-16 16:40:26 +0800

    added simple drag and drop added down arrow for index.html updated .gitignore and ignore folder

--- scripts/component.js
+++ scripts/component.js
@@ -6,6 +6,20 @@ class Component {
         this.image.src = assetSrc
     }
 
+    checkBoundingBox({x, y}) { // boolean, checks if x,y is within bounding box
+        let box = this.boundingBox
+        return (x >= box.x && y >= box.y) && (x <= box.x2 && y <= box.y2)
+    }
+
+    get boundingBox(){
+        return {
+            x:this.position.x,
+            y:this.position.y,
+            x2:this.position.x + this.image.width,
+            y2:this.position.y + this.image.height,
+        }
+    }
+
     draw(canvas) {
         canvas.getContext('2d').drawImage(this.image,this.position.x, this.position.y)
     }
--- scripts/simulator.js
+++ scripts/simulator.js
@@ -1,12 +1,61 @@
 class Simulator{
-    constructor(components = []) {
+    constructor(components = [], selectedComponent = undefined) {
       this.canvas = document.querySelector('canvas')
       this.context = this.canvas.getContext('2d')
       this.components = components
+      this.selectedComponent = selectedComponent
       this.canvas.width = 1260
       this.canvas.height = 700
+
+      this.canvas.addEventListener('mousedown', (e) => this.handleMouseDown(e))
+      this.canvas.addEventListener('mousemove', (e) => this.handleMouseMove(e))
+      this.canvas.addEventListener('mouseup', () => this.handleMouseUp())
     }
-  
+    
+    handleMouseDown(e) {
+      
+      this.selectedComponent = this.getPiece(this.getMousePosition(e))
+      const mousePosition = this.getMousePosition(e)
+      console.log(this.components[1].checkBoundingBox(mousePosition))
+      console.log(mousePosition.x)
+      console.log(e.clientX)
+
+      if (this.selectedComponent != undefined){
+        this.selectedComponent.offset = {
+          x: mousePosition.x - this.selectedComponent.position.x,
+          y: mousePosition.y - this.selectedComponent.position.y
+
+        }
+      }
+    }
+
+    handleMouseMove(e) {
+      const mousePosition = this.getMousePosition(e)
+
+      if(this.selectedComponent != null) {
+        this.selectedComponent.position.x = mousePosition.x - this.selectedComponent.offset.x
+        this.selectedComponent.position.y = mousePosition.y - this.selectedComponent.offset.y
+      }
+    }
+
+    handleMouseUp() {
+      this.selectedComponent = null
+    }
+
+    getPiece(loc){
+      for (let i = this.components.length - 1 ; i >= 0; i--) {
+        if(this.components[i].checkBoundingBox(loc)) return this.components[i]
+      }
+    }
+
+    getMousePosition(e) {
+      const rect = this.canvas.getBoundingClientRect()
+      return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+      }
+    }
+
     start() {
       this.animate()
     }
